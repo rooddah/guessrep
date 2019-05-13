03c5567471459909e42082293718ca4460c30c83
rooddah <rooddah@poczta.fm>
2019-05-13 19:19:16 +0200

    wip

---
+++ src/pages/BasePage.java
@@ -0,0 +1,14 @@
+package pages;
+
+import java.util.Scanner;
+
+public class BasePage {
+String abc = "";
+
+public static void main() {
+    Scanner in = new Scanner(System.in);
+    String aa = in.next();
+    int i = in.nextInt();
+
+}
+}
